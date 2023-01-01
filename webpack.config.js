d961bb4ddf78f88af8da8baa1f3cb7bde450cc70
Gilbert Jacque <gilbert@jacque.eu>
2023-01-01 23:27:44 +0100

    komplett

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     hot: true,
     open: true,
     static: ['./'],
+    compress: true, 
+    Inline: true, 
+    port: '8080'
     allowedhosts: ['.azurewebsites.net']
   },
   plugins: [
